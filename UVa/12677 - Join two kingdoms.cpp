b068184f5e056b46198afd6595302f7eb340035f
mavd09 <manuelalejandrovergaradiaz@iMac.local>
2015-09-21 02:32:18 -0500

    Solutions

--- UVa/12677 - Join two kingdoms.cpp
+++ UVa/12677 - Join two kingdoms.cpp
@@ -1,144 +1,140 @@
 #include <bits/stdc++.h>
 
-#define FILE 	1
-#define INPUT 	"input"
-#define OUTPUT 	"output"
+#define MP   make_pair
+#define PB   push_back
+#define FI   first
+#define SE   second
 
-#define MP 	    make_pair
-#define MT 	    make_tuple
-#define PB 	    push_back
-#define FI 	    first
-#define SE 	    second
+#define MAX     40100
 
-#define MAX 	int( 4e4+100 )
-#define INF 	INT_MAX
-#define EPS 	int(1e-7)
-#define MOD 	int(1e7+7)
-#define PI 	    acos(-1)
+#define INF  ( 1<<29 )
 
 typedef long long ll;
+typedef unsigned long long ull;
 
 using namespace std;
 
-int n, q;
-int fr, to;
-int H[ 3 ][ MAX ][ 3 ];
-int diam[ 3 ];
-vector< int > G[ 3 ][ MAX ];
-int seen[ MAX ];
+int N, Q;
+int maxDiameter, diameter[ 2 ], total[ 2 ], accum[ MAX ];
+vector< int > G[ 2 ][ MAX ];
+int d[ MAX ], minDist[ 2 ][ MAX ];
+bool visited[ MAX ];
 
-int dfs( int t, int v ) {
-    if( seen[ v ] == t ) return 0;
-    seen[ v ] = t;
-    for( int i = 0; i < int( G[ t ][ v ].size( ) ); i++ ) {
-        int u = G[ t ][ v ][ i ];
-        if( seen[ u ] != t ) {
-            int h = dfs( t, u )+1;
-            if( h > H[ t ][ v ][ 0 ] ) {
-                H[ t ][ v ][ 1 ] = H[ t ][ v ][ 0 ];
-                H[ t ][ v ][ 0 ] = h;
-            }
-            else {
-                H[ t ][ v ][ 1 ] = max( H[ t ][ v ][ 1 ], h );
-            }
-        }
+void initialize( ) {
+    total[ 0 ] = total[ 1 ] = 0;
+    for( int i = 0; i < MAX; i++ ) {
+        G[ 0 ][ i ].clear( );
+        G[ 1 ][ i ].clear( );
+        minDist[ 0 ][ i ] = minDist[ 1 ][ i ] = 0;
+        accum[ i ] = 0;
     }
-    return H[ t ][ v ][ 0 ];
 }
 
-void bfs( int t ) {
+pair< int, int > furthest( int id, int u ) {
+    
+    pair< int, int > ret( -1, -1 );
+    memset( d, -1, sizeof( d ) );
+    memset( visited, false, sizeof( visited ) );
     queue< int > q;
-    q.push( 1 );
-    seen[ 1 ] = t;
+    q.push( u );
+    d[ u ] = 0;
+    visited[ u ] = true;
+    
     while( !q.empty( ) ) {
-        int v = q.front( ); q.pop( );
-        diam[ t ] = max( diam[ t ], H[ t ][ v ][ 0 ]+H[ t ][ v ][ 1 ] );
-        for( int i = 0; i < int( G[ t ][ v ].size( ) ); i++ ) {
-            int u = G[ t ][ v ][ i ];
-            if( seen[ u ] != t ) {
-                seen[ u ] = t;
-                if( H[ t ][ v ][ 0 ]-1 == H[ t ][ u ][ 0 ] ) {
-                    H[ t ][ u ][ 1 ] = max( H[ t ][ u ][ 1 ], H[ t ][ v ][ 1 ]+1 );
-                    if( H[ t ][ u ][ 0 ] < H[ t ][ u ][ 1 ] ) swap( H[ t ][ u ][ 0 ], H[ t ][ u ][ 1 ] );
-                }
-                else {
-                    int cur =  H[ t ][ v ][ 0 ]+1;
-                    if( cur > H[ t ][ u ][ 0 ] ) {
-                        H[ t ][ u ][ 1 ] = H[ t ][ u ][ 0 ];
-                        H[ t ][ u ][ 0 ] = cur;
-                    }
-                    else {
-                        H[ t ][ u ][ 1 ] = max( H[ t ][ u ][ 1 ], cur );
-                    }
-                }
-                q.push( u );
-            }
+        u = q.front( ); q.pop( );
+        if( d[ u ] > ret.FI ) ret = MP( d[ u ], u );
+        for( int i = 0, v; i < int( G[ id ][ u ].size( ) ); i++ ) {
+            v = G[ id ][ u ][ i ];
+            if( visited[ v ] ) continue;
+            visited[ v ] = true;
+            d[ v ] = d[ u ]+1;
+            q.push( v );
         }
     }
+    
+    return ret;
+    
+}
+
+void fillMinDist( int id, int u ) {
+    
+    memset( d, -1, sizeof( d ) );
+    memset( visited, false, sizeof( visited ) );
+    queue< int > q;
+    q.push( u );
+    d[ u ] = 0;
+    visited[ u ] = true;
+    
+    while( !q.empty( ) ) {
+        u = q.front( ); q.pop( );
+        minDist[ id ][ u ] = max( minDist[ id ][ u ], d[ u ] );
+        for( int i = 0, v; i < int( G[ id ][ u ].size( ) ); i++ ) {
+            v = G[ id ][ u ][ i ];
+            if( visited[ v ] ) continue;
+            visited[ v ] = true;
+            d[ v ] = d[ u ]+1;
+            q.push( v );
+        }
+    }
+    
 }
 
 int main( ) {
-
-	if( FILE & 1 ) freopen( INPUT , "r", stdin  );
-	if( FILE & 2 ) freopen( OUTPUT, "w", stdout );
-
-    while( scanf( "%d %d", &n, &q ) != EOF ) {
-
-        for( int i = 0; i <= max( n, q ); i++ ) {
-            seen[ i ] = 0;
-            G[ 1 ][ i ].clear( );
-            G[ 2 ][ i ].clear( );
-            H[ 1 ][ i ][ 0 ] = H[ 1 ][ i ][ 1 ] = 0;
-            H[ 2 ][ i ][ 0 ] = H[ 2 ][ i ][ 1 ] = 0;
-            diam[ 1 ] = diam[ 2 ] = 0;
+    
+    ios_base::sync_with_stdio( 0 );
+    cin.tie( 0 );
+    cout.tie( 0 );
+    
+    while( cin >> N >> Q ) {
+        
+        initialize( );
+        for( int i = 1, u, v; i < N; i++ ) {
+            cin >> u >> v;
+            G[ 0 ][ u ].PB( v );
+            G[ 0 ][ v ].PB( u );
         }
-
-        for( int i = 1; i < n; i++ ) {
-            scanf( "%d %d", &fr, &to );
-            G[ 1 ][ fr ].PB( to );
-            G[ 1 ][ to ].PB( fr );
+        for( int i = 1, u, v; i < Q; i++ ) {
+            cin >> u >> v;
+            G[ 1 ][ u ].PB( v );
+            G[ 1 ][ v ].PB( u );
         }
-        for( int i = 1; i < q; i++ ) {
-            scanf( "%d %d", &fr, &to );
-            G[ 2 ][ fr ].PB( to );
-            G[ 2 ][ to ].PB( fr );
+        
+        int A, B, C, D;
+        pair< int, int > tmp;
+        
+        A = furthest( 0, 1 ).SE;
+        tmp = furthest( 0, A );
+        B = tmp.SE;
+        diameter[ 0 ] = tmp.FI;
+        
+        C = furthest( 1, 1 ).SE;
+        tmp = furthest( 1, C );
+        D = tmp.SE;
+        diameter[ 1 ] = tmp.FI;
+        
+        fillMinDist( 0, A ); fillMinDist( 0, B );
+        fillMinDist( 1, C ); fillMinDist( 1, D );
+        
+        maxDiameter = max( diameter[ 0 ], diameter[ 1 ] );
+        
+        sort( minDist[ 1 ]+1, minDist[ 1 ]+1+Q );
+        for( int i = 1; i <= Q; i++ ) accum[ i ] = accum[ i-1 ]+minDist[ 1 ][ i ];
+        
+        double ans = 0.0;
+        for( int i = 1; i <= N; i++ ) {
+            int lo = 1, hi = Q, mi;
+            while( lo <= hi ) {
+                mi = ( lo+hi )/2;
+                if( minDist[ 0 ][ i ]+1+minDist[ 1 ][ mi ] <= maxDiameter ) lo = mi+1;
+                else                                                        hi = mi-1;
+            }
+            ans += double( maxDiameter*hi )+double( accum[ Q ]-accum[ hi ] )+double( minDist[ 0 ][ i ]+1 )*double( Q-hi );
         }
-
-        double ans = 0;
-        memset( seen, 0, sizeof( seen ) );
-        dfs( 1, 1 );
-        dfs( 2, 1 );
-        memset( seen, 0, sizeof( seen ) );
-        bfs( 1 );
-        bfs( 2 );
-
-        int n1 = 0, n2 = 0, q1 = 0, q2 = 0;
-        for( int i = 1; i <= n; i++ ) {
-            if( H[ 1 ][ i ][ 0 ] < diam[ 2 ]/2 ) n1++;
-            else n2++;
-        }
-        for( int i = 1; i <= q; i++ ) {
-            if( H[ 2 ][ i ][ 0 ] < diam[ 1 ]/2 ) q1++;
-            else q2++;
-        }
-
-        cout << n1 << " " << n2 << " " << q1 << " " << q2 << endl;
-
-        for( int i = 1; i <= n; i++ ) {
-            if( H[ 1 ][ i ][ 0 ] < diam[ 2 ]/2 ) { }
-            else ans += ( H[ 1 ][ i ][ 0 ] )*q2;
-        }
-        for( int i = 1; i <= q; i++ ) {
-            if( H[ 2 ][ i ][ 0 ] < diam[ 1 ]/2 ) { }
-            else ans += ( H[ 2 ][ i ][ 0 ] )*n2;
-        }
-        ans += diam[ 2 ]*n1*q2;
-        ans += diam[ 1 ]*q1*n2;
-        ans += q2*n2;
-        ans /= n*q*1.0;
-        printf( "%.3f\n", ans );
-
+        ans /= double( N*Q );
+        cout << fixed << setprecision( 3 ) << ans << "\n";
+        
     }
-
+    
     return 0;
+    
 }
