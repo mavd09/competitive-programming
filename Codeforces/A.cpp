ba689e6d16b73b5934d5a119a55588b8e98d74f6
Manuel Vergara <mavd09@hotmail.com>
2017-02-10 23:08:30 -0500

    Templates

---
+++ Codeforces/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define PB          push_back
+#define PF          push_front
+#define MP          make_pair
+#define FI          first
+#define SE          second
+#define SIZE( A )   int( ( A ).size( ) )
+#define ALL( A )    ( A ).begin( ), ( A ).end( )
+#define ALLR( A )   ( A ).rbegin( ), ( A ).rend( )
+
+using namespace std;
+
+typedef long long           ll;
+typedef unsigned long long  ull;
+typedef long double         lf;
+typedef pair< int, int >    pii;
+typedef pair< ll, ll >      pll;
+typedef vector< bool >      vb;
+typedef vector< lf >        vd;
+typedef vector< ll >        vll;
+typedef vector< int >       vi;
+typedef vector< pii >       vpii;
+
+typedef complex< lf >       pt;
+
+const int MAXN = int( 1e5 );
+const int MOD  = int( 360 );
+const int oo   = INT_MAX;
+
+
+
+int main( ) {
+
+  #ifdef LOCAL
+    freopen( "input", "r", stdin );
+    //freopen( "output", "w", stdout );
+  #else
+    //freopen( "input", "r", stdin );
+    //freopen( "output", "w", stdout );
+    ios_base::sync_with_stdio( 0 );
+    cin.tie( 0 );
+  #endif
+
+
+
+  return 0;
+}
